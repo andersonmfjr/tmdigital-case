c5a75f5d999a6ed497d1c211fc0a3f15cce718cf
Anderson Feitosa <andersonmfjr@gmail.com>
2025-02-20 02:04:49 -0300

    feat: add login component scaffold

---
+++ apps/tm/src/app/pages/auth/ui/login/login.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent {}
