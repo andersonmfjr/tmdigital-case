d8ebe43d992f989a74a1071837991d4770c0b3c8
Anderson Feitosa <andersonmfjr@gmail.com>
2025-02-20 13:50:26 -0300

    feat: add login animations

--- apps/tm/src/app/pages/auth/ui/login/login.component.ts
+++ apps/tm/src/app/pages/auth/ui/login/login.component.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputComponent } from '../../../../shared/ui/input/input.component';
 import { ButtonComponent } from '../../../../shared/ui/button/button.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { 
+  animate, 
+  style, 
+  transition, 
+  trigger, 
+  query, 
+  stagger, 
+  keyframes 
+} from '@angular/animations';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +21,37 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('formAnimation', [
+      transition(':leave', [
+        query('.form-control', [
+          stagger(100, [
+            animate('0.5s ease-out', keyframes([
+              style({ 
+                opacity: 1, 
+                transform: 'translateX(0)',
+                offset: 0 
+              }),
+              style({ 
+                opacity: 0.5, 
+                transform: 'translateX(30px)',
+                offset: 0.3 
+              }),
+              style({ 
+                opacity: 0, 
+                transform: 'translateX(-100%)',
+                offset: 1 
+              })
+            ]))
+          ])
+        ]),
+        animate('0.3s ease-out', style({ 
+          transform: 'scale(0.8) translateY(-100%)',
+          opacity: 0 
+        }))
+      ])
+    ])
+  ]
 })
 export class LoginComponent {
   loginForm = new FormGroup({
@@ -21,9 +61,21 @@ export class LoginComponent {
 
   isLoading = false;
 
-  onSubmit() {
+  router = inject(Router);
+
+  async onSubmit() {
     if (this.loginForm.valid) {
-      console.log('Form submitted', this.loginForm.value);
+      this.isLoading = true;
+      
+      try {
+        await new Promise(resolve => setTimeout(resolve, 4000));
+        
+        await this.router.navigateByUrl('/auth/register');
+      } catch (error) {
+        console.error('Erro ao fazer login:', error);
+      } finally {
+        this.isLoading = false;
+      }
     }
   }
 
